3f8f708d501f017687c4ad1b5fdac1bec35c1d35
22406787 <ahachicha4@outlook.fr>
2025-09-24 15:56:02 +0200

    24/09

--- src/main/java/Guitar.java
+++ src/main/java/Guitar.java
@@ -1,4 +1,11 @@
 public class Guitar {
-    private String serialNumber,model;
+    private String serialNumber;
     private double price;
+    private GuitarSpec guitarSpec;
+
+    public Guitar(String serialNumber, double price, GuitarSpec guitarSpec) {
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.guitarSpec = guitarSpec;
+    }
 }
--- src/main/java/Instrument.java
+++ src/main/java/Instrument.java
@@ -1,4 +1,11 @@
 public class Instrument {
     private String serialNumber;
     private double price;
+    private InstrumentSpec instrumentSpec;
+
+    public Instrument(String serialNumber, double price, InstrumentSpec instrumentSpec) {
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.instrumentSpec = instrumentSpec;
+    }
 }
